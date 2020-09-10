e2359b11414c741e08e4c27d36d8aff9789c5da2
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2020-09-10 18:58:30 +0300

    Оптимизация изображения

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const csso = require("gulp-csso");
 const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
 
@@ -26,6 +29,41 @@ const styles = () => {
 }
 
 exports.styles = styles;
+
+//Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo(),
+    ]))
+}
+
+exports.images = images;
+
+//Webp
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"))
+}
+
+exports.createWebp = createWebp;
+
+//SVG sprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"))
+}
+
+exports.sprite = sprite;
+
 
 // Server
 
@@ -50,6 +88,10 @@ const watcher = () => {
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
-);
+const build = gulp.series(styles, images, createWebp, sprite)
+exports.build = build
+
+const watch = gulp.series(build, watcher)
+exports.watch = watch
+
+exports.start = gulp.series(watch, server)
